21f11877393c1f635190c7047791435eb34ef867
Diana Nthenya Odhiambo <109960883+Dianaodhiambo@users.noreply.github.com>
2022-12-10 19:11:34 +0000

    completed

--- db/migrate/20221210183323_change_data_type.rb
+++ db/migrate/20221210183323_change_data_type.rb
@@ -1,5 +1,5 @@
 class ChangeDataType < ActiveRecord::Migration[7.0]
   def change
-    change_column(:reviews, :rating, :integer)
+    change_column(:reviews, :rating, :string)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_09_053621) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_10_183323) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -25,7 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_09_053621) do
   create_table "reviews", force: :cascade do |t|
     t.string "username"
     t.string "comment"
-    t.integer "rating"
+    t.string "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "movie_id"
